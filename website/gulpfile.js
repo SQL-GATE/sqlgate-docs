97c55ab20456dee703cfd17bd219919d88372ee4
Thomas Jang <tom@axisj.com>
2018-05-28 17:52:36 +0900

    add gulp-sass

---
+++ website/gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// 필요한 모듈선언
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+
+function errorAlert(error) {
+  notify.onError({
+    title: "Gulp Error",
+    message: "Check your terminal",
+    sound: "Purr"
+  })(error); //Error Notification
+  console.log(error.toString()); //Prints Error to Console
+  this.emit("end"); //End function
+}
+
+/**
+ * watch
+ */
+gulp.task("scss-watch", function() {
+  gulp.watch(["./static/scss/**/*.scss"], ["scss"]);
+});
+
+/**
+ * SASS
+ */
+gulp.task("scss", function() {
+  gulp
+    .src("static/scss/custom.scss")
+    .pipe(plumber({ errorHandler: errorAlert }))
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("./static/css"));
+});
+
+// 걸프 기본 타스크
+gulp.task("default", ["scss-watch"], function() {
+  return true;
+});
