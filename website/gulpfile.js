7b14edb42948d82bcec8e13f7748adb42e5a2826
Kelly Kim <mein2555@gmail.com>
2018-08-01 14:50:52 +0900

    Footer 크로스 브라우징 (파폭 고마워!)

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,40 +1,40 @@
-"use strict";
+'use strict';
 
 // 필요한 모듈선언
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
 
 function errorAlert(error) {
   notify.onError({
-    title: "Gulp Error",
-    message: "Check your terminal",
-    sound: "Purr"
+    title: 'Gulp Error',
+    message: 'Check your terminal',
+    sound: 'Purr',
   })(error); //Error Notification
   console.log(error.toString()); //Prints Error to Console
-  this.emit("end"); //End function
+  this.emit('end'); //End function
 }
 
 /**
  * watch
  */
-gulp.task("scss-watch", function() {
-  gulp.watch(["./static/scss/**/*.scss"], ["scss"]);
+gulp.task('scss-watch', function() {
+  gulp.watch(['./static/scss/**/*.scss'], ['scss']);
 });
 
 /**
  * SASS
  */
-gulp.task("scss", function() {
+gulp.task('scss', function() {
   gulp
-    .src("static/scss/custom.scss")
-    .pipe(plumber({ errorHandler: errorAlert }))
-    .pipe(sass({ outputStyle: "compressed" }))
-    .pipe(gulp.dest("./static/css"));
+    .src('static/scss/custom.scss')
+    .pipe(plumber({errorHandler: errorAlert}))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./static/css'));
 });
 
 // 걸프 기본 타스크
-gulp.task("default", ["scss-watch"], function() {
+gulp.task('default', ['scss-watch'], function() {
   return true;
 });
